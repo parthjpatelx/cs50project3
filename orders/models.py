4ad17bab53b8829191a2f68c2d0a52f880d159d7
Parth <parthjpatelx@gmail.com>
2020-06-20 16:10:50 -0400

    first commit

---
+++ orders/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+
+#Added
+class Pizza_style(models.Model):
+    style = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.style}'
+
+class Size(models.Model):
+    size = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return f'{self.size}'
+
+# cheese = 0 toppings
+# special = 4 toppings
+
+class Pizza(models.Model):
+    type = models.ForeignKey(Pizza_style, on_delete=models.CASCADE, related_name='types')
+    # set toppings to 0 if toppings are not specified
+    toppings = models.IntegerField()
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='sizes')
+    price = models.FloatField(max_length=4)
+
+    def __str__(self):
+        return f"Pizza type: {self.type}, Toppings: {self.toppings}, Size: {self.size}, Price: {self.price}"
+
+class Topping(models.Model):
+    topping = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.topping}'
+
+#how to add extra cheese to a sub? Add an optional boolean?
+
+class Sub_type(models.Model):
+    type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.type}'
+
+class Sub(models.Model):
+    type = models.ForeignKey(Sub_type, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.FloatField(max_length=4)
+    # toppings = models.ManyToManyField(Toppings, blank=True)
+
+    def __str__(self):
+        return f'Type: {self.type}, Size = {self.size}, {self.price}'
+
+    
+class Pasta_type(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class PastaProtein(models.Model):
+    protein = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.protein}'
+
+class Pasta(models.Model):
+    name = models.ForeignKey(Pasta_type, on_delete=models.CASCADE)
+    price = price = models.FloatField(max_length=4)
+    protein = models.ForeignKey(PastaProtein, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Name: {self.name}, Price: {self.price}, Protein: {self.protein}'
+
+
+class Platter_type(models.Model):
+    type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.type}'
+
+class Platter(models.Model):
+    name = models.ForeignKey(Platter_type, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.FloatField(max_length=4)
+
+    def __str__(self):
+        return f'Name: {self.name}, Size: {self.size}, Price: {self.price}'
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField(max_length=4)
+
+    def __str__(self):
+        return f'Name: {self.name}, Price: {self.price}'
+
+class Cart(models.Model):
+    pizza= models.ManyToManyField(Pizza, blank=True)
+    subs= models.ManyToManyField(Sub, blank=True)
+    pasta= models.ManyToManyField(Pasta, blank=True)
+    salads= models.ManyToManyField(Salad, blank=True)
+    platters= models.ManyToManyField(Platter, blank=True)
---
+++ orders/views.py
@@ -0,0 +1,77 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
+from orders.models import Pizza_style, Pizza, Size
+
+
+def index(request):
+    # style_objects = Pizza_style.objects.all()
+    # style_names = []
+    # for style in style_objects:
+    #     name = style.__dict__['style']
+    #     style_names.append(name)
+
+    # context = {
+    #     'pizza_styles': style_names
+    # }
+    # return render(request, "orders/menu.html", context)
+
+    serialized = []
+    for pizza in Pizza.objects.all(): 
+        pie = pizza.__dict__
+        item = {'type': Pizza_style.objects.get(pk=pie['type_id']), 
+                'toppings':pie['toppings'], 
+                'size':Size.objects.get(pk=pie['size_id']),
+                'price': pie['price'],
+                'id' : pie['id']}
+        serialized.append(item)
+    context = {
+        'pizzas': serialized
+    }
+
+    return render(request, "orders/menu.html", context)
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/login.html", {"message": "Invalid credentials."})
+    else:
+        return render(request, "orders/login.html")
+
+def logout_view(request):
+      logout(request)
+      return render(request, "orders/login.html", {"message": "Logged out."})
+
+def register(request):
+    # note: add error pages so that users are not to debug page in case an exception is made (i..e if passwords dont match, django inherent form validation)
+    if request.method == 'POST':
+        try:
+            username = request.POST["username"]
+            password = request.POST["password"]
+            password_again = request.POST["password_again"]
+            email = request.POST["email"]
+        except KeyError:
+            return render(request, "orders/error.html", {"message": "Please enter all required fields"})
+        
+        if password != password_again:
+            # return render(request, "orders/error.html", {"message": "Passwords must match"})
+            raise ValidationError('passwords must match')    
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        login(request, user)
+
+        return HttpResponseRedirect(reverse("index"))
+    else: 
+        return render(request, "orders/register.html")
