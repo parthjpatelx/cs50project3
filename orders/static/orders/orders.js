5dbedf14a69674f679da6e6da972aa6db0b972e1
Parth <parth@Parths-MBP.home>
2020-06-26 20:22:33 -0400

    working on ajax request

--- orders/static/orders/orders.js
+++ orders/static/orders/orders.js
@@ -1,33 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    alert('hello');
     document.querySelectorAll('.pizza').forEach(link => {
         link.onclick = () => {
             alert('hello');
-            const request = new XMLHttpRequest();
-            request.open('POST', '/cart');
-            
-            request.onload = () => {
-                alert('request sent to server');
-                // Extract JSON data from request
-                const data = JSON.parse(request.responseText);
-  
-                // Update the result div
-                if (data.success) {
-                    alert('succesful response from server');
-                }
-                else {
-                    alert('error');
-                }
-            }
-
-            const data = new FormData();
-            var pizza_id = link.dataset.pizza;
-            data.append('pizza_id', pizza_id);
-
-            var csrftoken = Cookies.get("csrftoken");
-            request.setRequestHeader("X-CSRFToken", csrftoken);
-
-            request.send(data);
-            return false; 
         }
     }
     ); 
